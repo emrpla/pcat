be234c9be23d7aa24529f58f690d6f6e5b8f1450
Emre Pala <emrpla12@gmail.com>
2021-11-02 16:01:59 +0300

    Mongodb Created and started Crud operaitons

--- app.js
+++ app.js
@@ -1,29 +1,44 @@
 const express = require('express');
 const ejs = require('ejs');
 const path = require('path');
+const mongoose = require('mongoose');
+const Photo = require('./models/Photo');
 
 const app = express();
+
+// Connect DB
+mongoose.connect('mongodb://localhost/pcat-test-db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}); //Veritabanı yoksa oluşturur aynı zamanda
 
 // Template Engine
 app.set('view engine', 'ejs');
 
 // Middlewares
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Route
-app.get('/', (req, res) => {
-    //res.sendFile(path.resolve(__dirname, 'temp/index.html'));
-    res.render('index');
+app.get('/', async (req, res) => {
+    const photos = await Photo.find({});
+    res.render('index', {
+        photos: photos,
+    });
 });
 
 app.get('/about', (req, res) => {
-    //res.sendFile(path.resolve(__dirname, 'temp/index.html'));
     res.render('about');
 });
 
 app.get('/add', (req, res) => {
-    //res.sendFile(path.resolve(__dirname, 'temp/index.html'));
     res.render('add');
+});
+
+app.post('/photos', async (req, res) => {
+    await Photo.create(req.body);
+    res.redirect('/');
 });
 
 const port = 3000;
