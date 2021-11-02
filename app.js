58faaa5494db8609734a543425a2606e27e8148c
Emre Pala <emrpla12@gmail.com>
2021-11-03 00:53:51 +0300

    single photo page created

--- app.js
+++ app.js
@@ -28,6 +28,13 @@ app.get('/', async (req, res) => {
     });
 });
 
+app.get('/photos/:id', async (req, res) => {
+    const photo = await Photo.findById(req.params.id);
+    res.render('photo', {
+        photo: photo,
+    });
+});
+
 app.get('/about', (req, res) => {
     res.render('about');
 });
